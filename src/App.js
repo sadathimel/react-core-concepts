d7e47c32f159846f27ce6d6cc00234352f2481de
sadat <sadat.himel@gmail.com>
2020-08-22 00:26:33 +0600

    learn component and props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman sha"];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-      
+        <p>I am a React Person</p>
+        <Person name="Munna" job="football"></Person>
+        <Person name="Masum" job="Dorshok"></Person>
+        <Person></Person>
       </header>
+    </div>
+  );
+}
+function Person(props) {
+  return (
+    <div style={{border:'2px solid gold', width:'400px',margin:'10px',borderRadius:'10px'}}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job}</p>
     </div>
   );
 }
